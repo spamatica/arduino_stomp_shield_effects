d748e2180e9d5c678af6a43628cbc38c9e2bb1aa
Robert Jonsson <spamatica@gmail.com>
2015-02-11 20:03:00 +0100

    initial versions of stomp shield effects

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # arduino_stomp_shield_effects
 Effects for the Arduino Stomp shield
+http://www.openmusiclabs.com/projects/stomp-shield/
+
+This repository does not contain much of use yet.
+It's a sharing ground for Arduino stomp shield effects
+
+This far it contains two effects which was created in my efforts
+to create an octave down effect.
+
+* stomp_triangular_trigg - produces a mellow fuzz that may actually be of some use
+
+* stomp_triangular_octave - produces wierd noises, doesn't really work :)
+
+To install these effects clone this repository and copy the directories to your Arduino sketchbook directory. Or clone directly to the sketchbook if you feel that works for you.
+
+
---
+++ stomp_triangular_trigg/stomp_triangular_trigg.ino
@@ -0,0 +1,80 @@
+
+#include "StompShield.h"
+
+int outBuffer = 0x80;
+int output;
+int oldVal;
+int minVal = 0;
+int maxVal = 0;
+int currDirection = 0;
+int input;
+int average;
+
+void setup() {
+  StompShield_init(); // setup the arduino for the shield 
+  Serial.begin(9600); 
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }  
+}
+
+void loop() {
+//  delay(1000);
+//  Serial.print(input);
+//  Serial.print('_');
+//  Serial.print(currDirection);
+//  Serial.print('~');
+//  Serial.print(average);
+//  Serial.print('~');
+//  Serial.print(minVal);
+//  Serial.print('_');
+//  Serial.print(maxVal);
+//  Serial.print('>');
+//  Serial.print(output);
+//  Serial.print('\n');
+}
+
+ISR(TIMER1_OVF_vect) {  
+  // get ADC data
+  byte temp1 = ADCL; // you need to fetch the low byte first
+  byte temp2 = ADCH; // yes it needs to be done this way
+  input = ((temp2 << 8) | temp1) + 0x8000; // make a signed 16b value
+  
+  if (minVal==0) { // first time initialization;
+    oldVal = input;
+    output = input;
+  }
+  
+  if (input > maxVal)
+    maxVal = +1;
+  if (input < minVal)
+    minVal = +1;
+
+  
+//  average = ((maxVal-minVal)/2)+minVal;
+  if (input < 0) // going /
+    if (oldVal >= 0)
+      currDirection = 0;
+  if (input > 0) // going \
+    if (oldVal <= 0)
+      currDirection = 1;
+  
+  // produce output data based on currDirection
+  if (currDirection)
+    output+=200;
+  else
+    output-=200;
+  if (output < -5000) output=-5000;
+  if (output > 5000) output= 5000;
+  
+  oldVal = input;
+  // save data
+  outBuffer = output;
+  // output data
+  OCR1AL = ((outBuffer >> 8) + 0x80); 
+  OCR1BL = outBuffer; // output the bottom byte
+
+
+}
+
+
