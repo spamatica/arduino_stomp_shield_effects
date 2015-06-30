54b75c9d27e84111dfe0a0054369577766a5a355
Robert Jonsson <spamatica@gmail.com>
2015-06-30 22:12:19 +0200

    added a really nice chorus adapted from version found in PedalShield forum

---
+++ stomp_chorus1/stomp_chorus1.ino
@@ -0,0 +1,99 @@
+/* CHORUS for Stompshield adapted from pedal shield, see below for history and copyright */
+// Licensed under a Creative Commons Attribution 3.0 Unported License.
+// Based on rcarduino.blogspot.com previous work.
+// www.electrosmash.com/pedalshield
+ 
+
+#include "StompShield.h"
+
+#define B_MAX 100 // max value for rotary encoder input (min freq)
+#define B_MIN 1 // min value for rotary encoder input (max freq)
+
+#define MAX_DELAY 240
+#define MIN_DELAY 200
+
+uint16_t sDelayBuffer0[MAX_DELAY*2];
+uint16_t sDelayBuffer1[MAX_DELAY*2];
+unsigned int DelayCounter = 0;
+unsigned int Delay_Depth = 10;
+unsigned int count_up=1;
+int p;
+int POT0 = 1;
+
+
+unsigned int location = 0; // incoming data buffer pointer
+byte button; // button checking timer
+byte last_state; // last rotary encoder state
+byte counter = 1; // rotary encoder rotation counter (and start value)
+unsigned int fractional = 0x00; // fractional sample position
+int data_buffer; // temporary data storage to give a 1 sample buffer
+
+void checkButton()
+{
+  button--; // check buttons every so often
+  if (button == 0) {
+    byte temp3 = (PIND & 0x94); // mask off button pins
+    if (((last_state | temp3) & 0x10) == 0) { // falling edge on pin4
+      if ((temp3 & 0x04) == 0) { // low value on pin2
+        if (counter > B_MIN) counter--; // if not at min, decrement
+      }
+      else { // high value on pin2
+        if (counter < B_MAX) counter++; // if not at max, increment
+      }
+    }
+    last_state = (temp3 ^ 0x94); // invert buttons states and save
+    button = 0x20; // reset counter (determines how often buttons are checked)
+  }
+}
+
+void setup() {
+  StompShield_init(); // setup the arduino for the shield
+}
+
+void loop() {
+  // nothing happens here, all down in the interrupt
+}
+
+
+ISR(TIMER1_OVF_vect) { // all processing happens here
+
+  // output the last value calculated
+  OCR1AL = ((data_buffer + 0x8000) >> 8); // convert to unsigned, send out high byte
+  OCR1BL = data_buffer; // send out low byte
+  
+  // get ADC data
+  byte temp1 = ADCL; // you need to fetch the low byte first
+  byte temp2 = ADCH; // yes it needs to be done this way
+  int input = ((temp2 << 8) | temp1) + 0x8000; // make a signed 16b value
+  
+  checkButton();
+//  Delay_Depth=counter;
+ 
+  sDelayBuffer0[DelayCounter] = input;
+  POT0 = counter;
+  DelayCounter++;
+  if(DelayCounter >= Delay_Depth) 
+  {
+    DelayCounter = 0; 
+    if(count_up)
+    {
+       for(p=0;p<POT0+1;p++) 
+       sDelayBuffer0[Delay_Depth+p]=sDelayBuffer0[Delay_Depth-1]; 
+       Delay_Depth=Delay_Depth+POT0;
+       if (Delay_Depth>=MAX_DELAY)count_up=0;
+    }
+    else
+    {
+       Delay_Depth=Delay_Depth-POT0;
+       if (Delay_Depth<=MIN_DELAY)count_up=1;
+    }
+  }
+
+   
+ 
+  
+  // save value for playback next interrupt
+  data_buffer = sDelayBuffer0[DelayCounter];
+}
+
+
